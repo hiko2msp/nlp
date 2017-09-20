7865a93977b3e105bfba5cbf99af443a9d15457a
yokotanaohiko <iam4570@example.com>
2017-09-21 07:28:38 +0900

    デモプログラムを追加した

---
+++ lesson3/natsume_reader.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from collections import Counter
+import tensorflow as tf
+
+def _read_words(filename):
+    with open(filename) as f:
+        return f.read().split()
+
+def _build_vocab(token_list):
+    counter = Counter(token_list)
+    count_pairs = counter.most_common()
+    words, _ = list(zip(*count_pairs))
+    word_to_id = dict(zip(words, range(len(words))))
+    return word_to_id
+
+def _file_to_word_ids(token_list, word_to_id):
+    return [word_to_id[word] for word in token_list if word in word_to_id]
+
+def natsume_raw_data():
+    train_path = 'wagahai.txt'
+    valid_path = 'bochan.txt'
+    test_path = 'kokoro.txt'
+
+    word_to_id = _build_vocab(_read_words(train_path) + _read_words(valid_path))
+    train_data = _file_to_word_ids(_read_words(train_path) + _read_words(valid_path), word_to_id)
+    valid_data = _file_to_word_ids(_read_words(test_path), word_to_id)
+    test_data = _file_to_word_ids(_read_words(test_path), word_to_id)
+    vocabulary = len(word_to_id)
+    return train_data, valid_data, test_data, vocabulary
+
+def natsume_producer(raw_data, batch_size, num_steps, name=None):
+    '''
+    夏目漱石のデータをイテレートします
+    '''
+    with tf.name_scope(name, 'NATSUMEProducer', [raw_data, batch_size, num_steps]):
+        raw_data = tf.convert_to_tensor(raw_data, name="raw_data", dtype=tf.int32)
+        data_len = tf.size(raw_data)
+        batch_len = data_len // batch_size
+
+        data = tf.reshape(raw_data[0 : batch_size * batch_len], [batch_size, batch_len])
+        epoch_size = (batch_len - 1) // num_steps
+        assertion = tf.assert_positive(
+            epoch_size,
+            message="epoch_size == 0, decrease batch_size or num_steps"
+        )
+
+        with tf.control_dependencies([assertion]):
+            epoch_size = tf.identity(epoch_size, name="epoch_size")
+        i = tf.train.range_input_producer(epoch_size, shuffle=False).dequeue()
+        x = tf.strided_slice(data, [0, i * num_steps], [batch_size, (i + 1) * num_steps])
+        x.set_shape([batch_size, num_steps])
+        y = tf.strided_slice(data, [0, i * num_steps + 1], [batch_size, (i + 1) * num_steps + 1])
+        y.set_shape([batch_size, num_steps])
+        return x, y
+
+if __name__ == '__main__':
+    train_data, valid_data, test_data, _ = natsume_raw_data()
+    x, y = natsume_producer(train_data, 10, 10)
+    with tf.Session() as session:
+        result = session.run(x)
+    print(result)
---
+++ lesson3/predict_next_word.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import numpy as np
+import tensorflow as tf
+from natsume_reader import _build_vocab, _read_words
+from create_natsume_data import token_generator
+from natsume_word_lm import NATSUMEInput, MediumConfig
+
+word_to_id = _build_vocab(_read_words('wagahai.txt') + _read_words('bochan.txt'))
+id_to_word = {v:k for k, v in word_to_id.items()}
+config = MediumConfig()
+
+def get_model(input_data, reuse=False, num_steps=2):
+    batch_size=1
+    # モデルの準備
+    with tf.device('/cpu:0'): 
+        data_len = tf.size(input_data)
+        batch_len = data_len
+        data = tf.reshape(input_data[0 : batch_size * batch_len], [batch_size, batch_len])
+        initializer = tf.random_uniform_initializer(-config.init_scale,
+                                                    config.init_scale)
+        with tf.variable_scope("Model", reuse=reuse, initializer=initializer):
+
+            embedding = tf.get_variable(
+                "embedding", [config.vocab_size, config.hidden_size], dtype=tf.float32)
+            inputs = tf.nn.embedding_lookup(embedding, data)
+
+            cell = tf.contrib.rnn.BasicLSTMCell(
+                          config.hidden_size, forget_bias=0.0, state_is_tuple=True)
+            cell = tf.contrib.rnn.MultiRNNCell(
+                [cell for _ in range(config.num_layers)], state_is_tuple=True)
+            
+            state = cell.zero_state(batch_size, tf.float32)
+    
+            outputs = []
+            with tf.variable_scope("RNN", reuse=reuse):
+                for step in range(num_steps):
+                    if step > 0: tf.get_variable_scope().reuse_variables()
+                    (cell_output, state) = cell(inputs[:, step, :], state)
+
+                    outputs.append(cell_output)
+            output = tf.reshape(tf.concat(outputs, 1), [-1, config.hidden_size])
+
+            softmax_w = tf.get_variable(
+                "softmax_w", [config.hidden_size, config.vocab_size], dtype=tf.float32)
+            softmax_b = tf.get_variable("softmax_b", [config.vocab_size], dtype=tf.float32)
+
+            logits = tf.nn.xw_plus_b(output, softmax_w, softmax_b)
+            logits = tf.reshape(logits, [batch_size, -1, config.vocab_size])
+    return tf.reshape(tf.argmax(logits, axis=2), [-1])
+
+
+def predict_next_word(session, input_text, data, reuse=False):
+    input_token_list = list(token_generator(input_text))
+    input_data = [word_to_id[word] for word in input_token_list if word in word_to_id]
+
+    with tf.device('/cpu:0'): 
+        batch_size = 1
+
+        # データの準備
+        model = get_model(num_steps=len(input_data), reuse=reuse, input_data=data)
+
+        token_id_list = session.run(model, 
+            feed_dict={data: input_data}
+        )
+    
+    try:
+        return id_to_word[token_id_list[-1]]
+    except:
+        return '。'
+
+if __name__ == '__main__':
+    data = tf.placeholder(tf.int32, shape=[None])
+    model = get_model(data, reuse=False)
+    saver = tf.train.Saver()
+    with tf.Session() as session:
+        ckpt = tf.train.get_checkpoint_state('./result')
+        if ckpt and tf.train.checkpoint_exists(ckpt.model_checkpoint_path):
+            
+            saver.restore(session, ckpt.model_checkpoint_path)
+
+        input_text = '吾輩は'
+        next_word = ''
+        while next_word != '。':
+            print(input_text)
+            next_word = predict_next_word(session, input_text, data, reuse=True)
+            if input_text.endswith(next_word):
+                input_text += '。'
+                break
+            input_text = input_text + next_word
+        print(input_text)
