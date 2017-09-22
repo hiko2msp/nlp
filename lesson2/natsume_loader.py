d9b75686d1f93c25c2c49bbec02b046000b10684
yokotanaohiko <iam4570@example.com>
2017-09-22 20:17:37 +0900

    fix

--- lesson2/natsume_loader.py
+++ lesson2/natsume_loader.py
@@ -5,9 +5,10 @@ import re
 import zipfile
 from janome.tokenizer import Tokenizer
 
-ruby_match = re.compile(r'《.*》')
-brace_match = re.compile(r'［.*］')
-stop_words = re.compile(r'[「。、」\r]')
+ruby_match = re.compile(r'《.*?》')
+brace_match = re.compile(r'［.*?］')
+stop_words = re.compile(r'[「、」\r]')
+explanation_match = re.compile(r'-{10,}[^-]*?-{10,}')
 
 def read_data(filename):
     """Extract the first file enclosed in a zip file as a list of words."""
@@ -17,14 +18,16 @@ def read_data(filename):
         text = ruby_match.sub('', text)
         text = brace_match.sub('', text)
         text = stop_words.sub('', text)
+        text = explanation_match.sub('', text)
     return text
 
 def token_generator(text):
     tokenizer = Tokenizer()
     for text_line in text.split('\n'):
-        for token in tokenizer.tokenize(text_line):
-            if token.part_of_speech.split(',')[0] == '名詞':
-                yield token.surface
+        for sentense in text_line.split('。'):
+            for token in tokenizer.tokenize(sentense):
+                if token.part_of_speech.split(',')[0] == '名詞':
+                    yield token.surface
 
 if __name__ == '__main__':
     text = read_data('789_ruby_5639.zip')
