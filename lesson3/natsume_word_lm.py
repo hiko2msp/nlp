2004fb3652ea69ba79f4bf4628abf7a4be0e9ddb
iam4570 <iam4570@yahoo.co.jp>
2017-10-03 06:57:02 +0900

    fix

--- lesson3/natsume_word_lm.py
+++ lesson3/natsume_word_lm.py
@@ -297,16 +297,16 @@ class SmallConfig(object):
 
 class MediumConfig(object):
   """Medium config."""
-  init_scale = 0.04
+  init_scale = 0.1
   learning_rate = 1.0
-  max_grad_norm = 10
+  max_grad_norm = 5
   num_layers = 2
-  num_steps = 35
-  hidden_size = 1500
-  max_epoch = 14
-  max_max_epoch = 55
-  keep_prob = 0.35
-  lr_decay = 1 / 1.15
+  num_steps = 20
+  hidden_size = 200
+  max_epoch = 4
+  max_max_epoch = 13
+  keep_prob = 1.0
+  lr_decay = 0.5
   batch_size = 20
   vocab_size = 10000
   rnn_mode = BLOCK
